c1822d8b866772814e874a727f61007b51e46d51
Gabrielle Brandt <110957615+brandt47@users.noreply.github.com>
2025-01-21 21:47:45 -0700

    Project half set up (node, express and connected mongo) (#2)
    * setting up proj
    
    * tsconfig
    
    * more setup
    
    * connected to mongodb

---
+++ server/src/server.ts
@@ -0,0 +1,34 @@
+import express, {Express, Request, Response} from 'express';
+import cors from 'cors';
+import {config} from './config';
+import mongoose from 'mongoose';
+
+const PORT = config.server.port;
+
+const app:Express = express();
+
+app.use(express.json());
+app.use(cors());
+
+(async function startUp() {
+    try {
+        await mongoose.connect(config.mongo.url, {w:"majority", retryWrites:true, authMechanism:"DEFAULT"});
+
+        console.log("Connection to MongoDB successfully made");
+
+
+        app.get("/health", (req:Request, res:Response) => {
+            res.status(200).json({message: "Server is running properly"});
+        })
+        
+        app.listen(PORT, () => {
+            console.log(`Server listening on port ${PORT}`);
+        })
+
+        
+    } catch (error) {
+        console.error('Error connecting to the database: ', error);
+    }
+
+})();
+
