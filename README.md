b34a9ef1edfbe77ba1e92ea8684a1bb87170eb23
Gabrielle Brandt <110957615+brandt47@users.noreply.github.com>
2025-01-26 13:39:05 -0700

    Added user friendly errors (#4)
    * added gitignore
    
    * excluded launch.json from gitignore
    
    * Added user friendly errors, working on getting validation to work properly

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # CMPT315
 CMPT 315 Project
 2025
+ 
+
+steps to run:
+cd into server folder
+in vscode terminal type: npm run dev
--- server/src/routes/AuthRoutes.ts
+++ server/src/routes/AuthRoutes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import AuthController from '../controllers/AuthController';
+import {Schemas, ValidateSchema} from '../middlewares/Validation';
 
 /*Here we are telling our Express application that we want a post request that can handle /register*/
 //whenever we get /register it will be handled by AuthController
@@ -8,5 +9,5 @@ import AuthController from '../controllers/AuthController';
 const router = express.Router();
 
 router.post("/register", AuthController.handleRegister);
-
+// ValidateSchema(Schemas.user.create),
 export = router;
