330a2b222f1308c324798bc16377e22d4941fb5d
Gabrielle Brandt <110957615+brandt47@users.noreply.github.com>
2025-01-27 23:00:12 -0700

    Login beginning to sorta work (#9)

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,29 @@
-
+import { useEffect, useState } from 'react';
+import HomePage from './pages/HomePage'
 import './App.css'
+import { User } from './models/User';
 
 function App() {
+
+  const [displayLogin, setDisplayLogin] = useState<boolean>(true);
+  //user undefined by default
+  const [loggedInUser, setLoggedInUser] = useState<User>();
+
+  const updateLoggedInUser = (user: User) => {
+    setLoggedInUser(user);
+  }
+
+  useEffect(() => {
+    console.log(loggedInUser);
+  }, [loggedInUser])
 
   return (
     <>
       <div>
-      <h1 className="text-amber-400"> HELLO! DO YOU WORK???</h1>   
+        <h1 className="text-amber-400"> HELLO! DO YOU WORK???</h1>
+        <HomePage displayLogin={displayLogin} updateLoggedInUser={updateLoggedInUser}/>
       </div>
-     
+
     </>
   )
 }
