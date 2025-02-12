058fc4417bf856404168061cd12aefa0a89641f8
Ben Morley <benjaminamorley@gmail.com>
2025-02-12 16:53:15 -0700

    Calendar made

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,55 +1,57 @@
-import { useEffect, useState } from 'react';
-import HomePage from './pages/HomePage'
-import './App.css'
-import { User } from './models/User';
+import { useEffect, useState } from "react";
+import HomePage from "./pages/HomePage";
+import "./App.css";
+import { User } from "./models/User";
+import Calendar from "./pages/Calendar";
 // import { SignUpForm } from './features/authentication/SignUpForm/SignUpForm';
 
 function App() {
-
   const [displayLogin, setDisplayLogin] = useState<boolean>(true);
   //user undefined by default
   const [loggedInUser, setLoggedInUser] = useState<User>();
-  
 
-  const[displaySignUp, setDisplaySignUp] = useState<boolean>(true);
+  const [displaySignUp, setDisplaySignUp] = useState<boolean>(true);
   const [signedUpUser, setSignedUpUser] = useState<User>();
-  
 
   const updateLoggedInUser = (user: User) => {
     setLoggedInUser(user);
     setDisplayLogin(false);
-  }
-  const signUpNewUser = (user:User)=> {
+  };
+  const signUpNewUser = (user: User) => {
     setSignedUpUser(user);
     setDisplaySignUp(false);
-  }
+  };
 
   useEffect(() => {
     console.log(loggedInUser);
-  }, [loggedInUser])
+  }, [loggedInUser]);
 
   useEffect(() => {
     console.log(signedUpUser);
-  }, [signedUpUser])
-  
+  }, [signedUpUser]);
+
   return (
     <>
       <div>
-
         {/* <h1 className="text-amber-400"> HELLO! DO YOU WORK???</h1> */}
 
-        <HomePage displayLogin={displayLogin} updateLoggedInUser={updateLoggedInUser} signUpNewUser={signUpNewUser} displaySignUp={displaySignUp}
-        
-        />
+        {/* <HomePage
+          displayLogin={displayLogin}
+          updateLoggedInUser={updateLoggedInUser}
+          signUpNewUser={signUpNewUser}
+          displaySignUp={displaySignUp}
+        /> */}
         {/* <SignUpForm/> */}
-        </div>
+
+        <Calendar />
+
+
+      </div>
       {/* <div>
         <HomePage displaySignUp={displaySignUp} updateSignUpUser={updateSignUpUser} displaySignUp={true}/>
       </div> */}
-
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ app/src/pages/Calendar.tsx
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+import {
+  format,
+  startOfMonth,
+  endOfMonth,
+  startOfWeek,
+  endOfWeek,
+  addDays,
+  isSameMonth,
+  isSameDay,
+  addMonths,
+} from "date-fns";
+import "./styles/calendar.css";
+
+const Calendar: React.FC = () => {
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const renderHeader = () => {
+    const dateFormat = "MMMM yyyy";
+
+    return (
+      <div className="header row flex-middle">
+        <div className="col col-start">
+          <button className="icon" onClick={prevMonth}>
+            &lt;
+          </button>
+        </div>
+        <div className="col col-center">
+          <span>{format(currentMonth, dateFormat)}</span>
+        </div>
+        <div className="col col-end">
+          <button className="icon" onClick={nextMonth}>
+            &gt;
+          </button>
+        </div>
+      </div>
+    );
+  };
+
+  const renderDays = () => {
+    const days = [];
+    const dateFormat = "EEEE";
+    const startDate = startOfWeek(currentMonth);
+
+    for (let i = 0; i < 7; i++) {
+      days.push(
+        <div className="col col-center" key={i}>
+          {format(addDays(startDate, i), dateFormat)}
+        </div>
+      );
+    }
+
+    return <div className="days row">{days}</div>;
+  };
+
+  const renderCells = () => {
+    const monthStart = startOfMonth(currentMonth);
+    const monthEnd = endOfMonth(monthStart);
+    const startDate = startOfWeek(monthStart);
+    const endDate = endOfWeek(monthEnd);
+
+    const rows = [];
+    let days = [];
+    let day = startDate;
+    let formattedDate = "";
+
+    while (day <= endDate) {
+      for (let i = 0; i < 7; i++) {
+        formattedDate = format(day, "d");
+        const cloneDay = day;
+        days.push(
+          <div
+            className={`col cell ${
+              !isSameMonth(day, monthStart)
+                ? "disabled"
+                : isSameDay(day, selectedDate)
+                ? "selected"
+                : ""
+            }`}
+            key={day.toString()}
+            onClick={() => onDateClick(cloneDay)}
+          >
+            <span className="number">{formattedDate}</span>
+            <span className="bg">{formattedDate}</span>
+          </div>
+        );
+        day = addDays(day, 1);
+      }
+      rows.push(
+        <div className="row" key={day.toString()}>
+          {days}
+        </div>
+      );
+      days = [];
+    }
+    return <div className="body">{rows}</div>;
+  };
+
+  const onDateClick = (day: Date) => {
+    setSelectedDate(day);
+  };
+
+  const nextMonth = () => {
+    setCurrentMonth(addMonths(currentMonth, 1));
+  };
+
+  const prevMonth = () => {
+    setCurrentMonth(addMonths(currentMonth, -1));
+  };
+
+  return (
+    <div className="calendar">
+      {renderHeader()}
+      {renderDays()}
+      {renderCells()}
+    </div>
+  );
+};
+
+export default Calendar;
